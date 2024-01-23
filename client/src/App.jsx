768ca3049109eaad70810de60cb517c76e27e9c9
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-23 15:30:41 +0530

    routing completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 
-
+import {BrowserRouter , Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
   
 
   return (
-    <>
-    <h1>app</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/about' element={<About/>}/>
+<Route path='/signup' element={<SignUp/>}/>
+<Route path='/signin' element={<Signin/>}/>
+<Route path='/dashboard' element={<Dashboard/>}/>
+<Route path='/projects' element={<Projects/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div>
+      <h1>project</h1>
+    </div>
+  )
+}
+
+export default Projects
