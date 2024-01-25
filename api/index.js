eb8058eb287f55a2435bf1303803e05392481f6d
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-25 15:54:21 +0530

    api route created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express  from "express";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouters from './routes/user.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO)
@@ -18,3 +19,5 @@ const app=express()
 app.listen(3000,()=>{
     console.log(`app is running at 3000`)
 })
+
+app.use('/api/user', userRouters)
