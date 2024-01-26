3d8629cc42fb476e4de4b9d841c2d15f47038794
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-26 19:58:05 +0530

    req.body obtained

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,4 @@
+export const signup =async(req,res)=>{
+
+    console.log(req.body);
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express  from "express";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouters from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO)
@@ -15,9 +16,11 @@ mongoose.connect(process.env.MONGO)
 
 const app=express()
 
-
+ app.use(express.json())
 app.listen(3000,()=>{
     console.log(`app is running at 3000`)
 })
 
 app.use('/api/user', userRouters)
+
+app.use('/api/auth', authRoutes)
