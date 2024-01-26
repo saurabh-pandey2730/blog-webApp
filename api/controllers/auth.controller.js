1c71c9d2f603bb6f328ff72ba019aa1240a6e905
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-26 20:16:28 +0530

    signup data stored to databse

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,24 @@
+import User from "../models/user.model.js"
+
 export const signup =async(req,res)=>{
 
-    console.log(req.body);
+    const {username, email , password } = req.body
+
+    if(!username || !email  || !password || username === '' || email === '' || password === ''){
+
+        return res.status(400).json({message:'All fields are required'})
+    }
+    const newUser  = new User ({
+        username,
+        email,
+        password,
+    })
+    try{
+        await newUser.save()
+    res.json('succesfully signup')
+    } catch(error){
+        res.status(500).json({message:error.message})
+
+    }
+    
 }
