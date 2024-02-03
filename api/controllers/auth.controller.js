fd64baf28134affe3a0efb6efbb7bb1fa8e4a9b7
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-03 13:39:57 +0530

    create sign api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
-import User from "../models/user.model.js"
-   import  bcryptjs from "bcryptjs"
+import User from "../models/user.model.js" 
+import  bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import  jwt  from 'jsonwebtoken'
+
+
 export const signup =async(req,res , next)=>{
 
     const {username, email , password } = req.body
@@ -30,4 +33,33 @@ export const signup =async(req,res , next)=>{
 
     }
     
+}
+
+export const signin=async(req,res,next)=>{
+const {email,password}=req.body
+if(!email||!password || email ==='' || password ===""){
+next(errorHandler(400, 'All field are required'))
+}
+try{
+    const validUser=await User.findOne({email})
+      if (!validUser) {
+         return next(errorHandler(404,"Invalid Email or Password"))
+      }
+      const  isValidPassord=bcryptjs.compareSync(password,validUser.password)
+      if(!isValidPassord){
+          return next(errorHandler(400,'Invalid  Password'))
+      }
+      const token=jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
+      const {password:pass,  ...others}=validUser._doc;
+      
+      res.status(200)
+      .cookie('access_token',token,{
+        httpOnly:true,
+      })
+      .json(others)
+      
+}catch(error){
+ next(error)
+}
+
 }
