986c485701e5f45caedf3419033eb2a75095826d
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-05 13:38:23 +0530

    store updated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -13,9 +13,9 @@ const persistConfig ={
 }
 const persistedReducer =persistReducer(persistConfig, rootReducer)
 export const store = configureStore({
-  reducer: persistReducer,
-  middleware : (getDfaultMiddleware)=>{
-    getDfaultMiddleware({serializableCheck:false})
+  reducer: persistedReducer,
+  middleware : (getDefaultMiddleware)=>
+    getDefaultMiddleware({serializableCheck:false})
   }
-});
+);
 export const persistor= persistStore(store);
