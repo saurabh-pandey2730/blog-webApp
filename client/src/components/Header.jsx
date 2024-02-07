9acabff5471d5fae0c33b02e43811bfe7ac7c5b9
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-07 12:20:31 +0530

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Button, Navbar, TextInput , Avatar,Dropdown } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from  'react-icons/ai'
 import {FaMoon} from  'react-icons/fa'
+import { useSelector } from 'react-redux'
  
 import React from 'react'
 
 export default function Header() {
     const path = useLocation().pathname
+    const {currentUser} = useSelector((state)=> state.user)
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -27,9 +29,35 @@ export default function Header() {
                 <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                     <FaMoon/>
                 </Button>
-                <Link to='/sign-up'>
+                 {currentUser ? (
+                  <Dropdown 
+                  arrowIcon={false}
+                  inline
+                  label={
+                    <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                  }
+                  >
+                    <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span  className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                        <Link to ={'/dashboard?tab=profile'}>
+                            <Dropdown.Item>
+                                Profile
+                            </Dropdown.Item>
+                         </Link>  
+                          <Dropdown.Divider />
+                          <Dropdown.Item>
+                                SignOut
+                            </Dropdown.Item>
+                      </Dropdown.Header>
+                  </Dropdown>
+
+                 ):(
+                    <Link to='/sign-up'>
                     <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
                 </Link>
+
+                 )}
                 <Navbar.Toggle/>
             </div>
                 <Navbar.Collapse>
