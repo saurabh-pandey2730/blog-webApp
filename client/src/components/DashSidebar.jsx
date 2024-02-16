c5359c2e58a867b945645925d3d76f0803a54894
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-16 17:00:49 +0530

    added posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,13 +1,15 @@
 
 import {Sidebar} from 'flowbite-react'
-import {HiUser , HiArrowSmRight}  from 'react-icons/hi';
+import {HiUser , HiArrowSmRight, HiDocumentText }  from 'react-icons/hi';
 import {useEffect, useState} from 'react'
 import {useLocation, Link} from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 export default function DashSidebar() {
   const dispatch = useDispatch()
     const location = useLocation()
+    const { currentUser } = useSelector((state) => state.user);
     const [tab,setTab]=useState('')
     useEffect(()=>{
         const urlParams = new URLSearchParams(location.search);
@@ -34,10 +36,21 @@ export default function DashSidebar() {
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-         <Sidebar.ItemGroup>
+         <Sidebar.ItemGroup  className='flex flex-col gap-1'>
             <Link to='/dashboard?tab=profile' >
-                <Sidebar.Item icon={HiUser}  active={tab==='profile'} label={'User'} labelColor='dark' as ='div' >Profile</Sidebar.Item>
+                <Sidebar.Item icon={HiUser}  active={tab==='profile'} label={ currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as ='div' >Profile</Sidebar.Item>
             </Link>
+            {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >Sign Out</Sidebar.Item>
          </Sidebar.ItemGroup>  
       </Sidebar.Items>       
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import {useState,useEffect} from  'react'
 import {useLocation} from  'react-router-dom'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
-
+import DashPosts from '../components/DashPosts'
   export default function Dashboard() {
   const location = useLocation()
   const [tab,setTab]=useState('')
@@ -23,6 +23,8 @@ import DashProfile from '../components/DashProfile'
       
            {/* Profile... */}
             {tab ==='profile' && <DashProfile/>}
+           {/* Posts... */}
+            {tab ==='posts' && <DashPosts/>}
      </div>
   )
 }
