7e84ece7b45e20b773a588607357e4713eec51e9
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-24 11:31:30 +0530

    comment api route completed and intigrated with frontend

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,55 +1,68 @@
-import {Link} from 'react-router-dom'
- import {useSelector} from 'react-redux'
- import   {Textarea , Button , Alert } from  'flowbite-react'
+import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
+import { set } from 'mongoose';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
- import { useState } from 'react';
-export default function CommentSection({postId}) {
+export default function CommentSection({ postId }) {
+  const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState(null);
-   const {currentUser} =useSelector(state=>state.user)
 
-   const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (comment.length > 200) {
-      return;
-    }
-    try {
-      const res = await fetch('/api/comment/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          content: comment,
-          postId,
-          userId: currentUser._id,
-        }),
-      });
-      const data = await res.json();
-      if (res.ok) {
-        setComment('');
-        setCommentError(null);
-      }
-    } catch (error) {
-      setCommentError(error.message);
-    }
-  };
-    return (
-    <div className='max-w-2xl mx-auto w-full p-3 '>
-          {currentUser ?
-           (
-            <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
-                <p>Signed in as:</p>
-                <img className='h-5 w-5 object-cover rounded-full' src={currentUser.profilePicture} alt="User" />
-                <Link to={'/dashboard?tab=profile'} className='text-xs text-cyan-600 hover:underline'>@{currentUser.username}</Link>
-            </div>
-          ):(
-            <div className='text-sm text-teal-500 my-5 flex gap-1'> 
-             you must be signed in to comment
-            <Link className='text-blue-500 hover:underline' to={'/sign-in'}> Sign In</Link> 
-            </div>
-          )}
-           {currentUser && (
+  const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (comment.length > 200) {
+          return;
+        }
+        try {
+          const res = await fetch('/api/comment/create', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              content: comment,
+              postId,
+              userId: currentUser._id,
+            }),
+          });
+          console.log(res)
+          const data = await res.json();
+          if (res.ok) {
+            setComment('');
+            setCommentError(null);
+          }
+        } catch (error) {
+          setCommentError(error.message);
+        }
+      };
+      
+  return (
+    <div className='max-w-2xl mx-auto w-full p-3'>
+      {currentUser ? (
+        <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
+          <p>Signed in as:</p>
+          <img
+            className='h-5 w-5 object-cover rounded-full'
+            src={currentUser.profilePicture}
+            alt=''
+          />
+          <Link
+            to={'/dashboard?tab=profile'}
+            className='text-xs text-cyan-600 hover:underline'
+          >
+            @{currentUser.username}
+          </Link>
+        </div>
+      ) : (
+        <div className='text-sm text-teal-500 my-5 flex gap-1'>
+          You must be signed in to comment.
+          <Link className='text-blue-500 hover:underline' to={'/sign-in'}>
+            Sign In
+          </Link>
+        </div>
+      )}
+      {currentUser && (
         <form
           onSubmit={handleSubmit}
           className='border border-teal-500 rounded-md p-3'
@@ -60,11 +73,10 @@ export default function CommentSection({postId}) {
             maxLength='200'
             onChange={(e) => setComment(e.target.value)}
             value={comment}
-            
           />
           <div className='flex justify-between items-center mt-5'>
             <p className='text-gray-500 text-xs'>
-            {200 - comment.length}  characters remaining
+              {200 - comment.length} characters remaining
             </p>
             <Button outline gradientDuoTone='purpleToBlue' type='submit'>
               Submit
@@ -78,5 +90,5 @@ export default function CommentSection({postId}) {
         </form>
       )}
     </div>
-  )
+  );
 }
