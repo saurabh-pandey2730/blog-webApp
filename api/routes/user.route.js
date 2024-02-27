82efd9363a47e002177d8d8937ebd19c6b368c41
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-02-27 11:06:02 +0530

    comment funtionality completed

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser,signout, test ,updateUser, getUsers} from '../controllers/user.controller.js';
+import { deleteUser,signout, test ,updateUser, getUsers, getUser} from '../controllers/user.controller.js';
  import { verifyToken } from '../utils/verifyUser.js';
 const router =express.Router()
 
@@ -8,5 +8,6 @@ router.put('/update/:userId', verifyToken, updateUser)
  router.delete('/delete/:userId' , verifyToken,deleteUser)
  router.post('/signout' , signout)
  router.get('/getusers', verifyToken, getUsers);
+ router.get('/:userId', getUser);
 
 export default router
