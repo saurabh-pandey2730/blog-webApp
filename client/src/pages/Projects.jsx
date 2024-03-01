bab9e43f9a2b3dbbb4f554ef7ca12e34d4941412
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-03-01 12:45:24 +0530

    project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 function Projects() {
   return (
-    <div>
-      <h1>project</h1>
-    </div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+    <h1 className='text-3xl font-semibold dark:text-white'>Projects</h1>
+    <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+    <CallToAction />
+  </div>
   )
 }
 
